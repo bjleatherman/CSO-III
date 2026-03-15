49d07959452274f90139c1b3cf854a473423339a
bjleatherman <bjleatherman@gmail.com>
2026-03-15 14:33:06 -0400

    feat: update process_turn method to include player_id and validate spawn requests

--- Engine/state_manager.py
+++ Engine/state_manager.py
@@ -3,6 +3,7 @@ from Model.game_rules import GameRules
 from Model.api_models import *
 from Model.domain_models import Address, Cell
 from uuid import uuid4
+from Engine.validators import ActionValidator
 
 class StateManager:
 
@@ -18,6 +19,20 @@ class StateManager:
     def get_current_state(self):
         return self.history.get_current_game_state()
     
-    def process_turn(self, request: FullTurnRequest) -> GameHistory:
+    def process_turn(self, turn: FullTurnRequest, player_id:int) -> GameHistory:
 
         candidate_state = self.history.create_next_state()
+        previous_state = self.get_current_state()
+        rules=self.history.game_rules
+
+        candidate_turn_num = candidate_state.turn_number
+        previous_turn_num = previous_state.turn_number
+
+        # Check if requesting spawn
+        if turn.spawn is not None:
+            ActionValidator.validate_spawn(
+                candidate_state=candidate_state,
+                SpawnRequest=turn.spawn,
+                board=self.history.board,
+                player_id=player_id
+                )
